9c320c7f88fc60e6debeed6079241b9c8b3a7f7f
unknown <t2kasa02@students.oamk.fi>
2024-01-17 10:37:57 +0200

    kt1 valmis

---
+++ kt1/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int game(int maxnum);
+
+
+int main()
+{
+    int arvaustenMaara = game(40);
+
+    std::cout << "Onneksi olkoon! Arvasit oikein. Arvausten määrä: " << arvaustenMaara << std::endl;
+
+    return 0;
+}
+int game(int maxnum) {
+    srand(static_cast<unsigned int>(time(0)));
+
+    int etsittavaLuku = rand() % maxnum + 1;
+
+    int pelaajanArvaus;
+
+    int arvaustenMaara = 0;
+
+    do {
+        std::cout << "Arvaa luku väliltä 1-" << maxnum << ": ";
+        std::cin >> pelaajanArvaus;
+        if (pelaajanArvaus < etsittavaLuku) {
+            std::cout << "Luku on suurempi." << std::endl;
+        } else if (pelaajanArvaus > etsittavaLuku) {
+            std::cout << "Luku on pienempi." << std::endl;
+        } else {
+            std::cout << "Oikea vastaus!" << std::endl;
+        }
+
+
+        arvaustenMaara++;
+
+    } while (pelaajanArvaus != etsittavaLuku);
+
+
+    return arvaustenMaara;
+}
